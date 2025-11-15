41a30305b3e73f464d6931d35e0868526eb661da
LeeBate <harukaxd02@gmail.com>
2025-11-15 12:30:51 +0700

    set up project and ui video call real time

---
+++ app/(clerk)/layout.tsx
@@ -0,0 +1,7 @@
+const ClerkLayOut = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex items-center justify-center h-[90vh]">{children}</div>
+  );
+};
+
+export default ClerkLayOut;
