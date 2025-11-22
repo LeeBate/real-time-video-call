eeb9fad4dc1ba51c7b8886f2beed93616303c506
LeeBate <harukaxd02@gmail.com>
2025-11-22 12:55:47 +0700

    update ui show camera and mic

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
   const router = useRouter();
   const { userId } = useAuth();
   return (
-    <div className="sticky top-0 border border-b-primary/10">
+    <div className="sticky top-0 border border-b-primary/10 bg-white">
       <Container>
         <div className="flex justify-between">
           <div
--- components/videoCall.tsx
+++ components/videoCall.tsx
@@ -2,17 +2,77 @@
 
 import { useSocket } from "@/context/SocketContext";
 import VideoContainer from "./videoContainer";
+import { useCallback, useState } from "react";
+import {
+  MdCallEnd,
+  MdMic,
+  MdMicOff,
+  MdVideocam,
+  MdVideocamOff,
+} from "react-icons/md";
 
 const VideoCall = () => {
   const { localStream } = useSocket();
+  const [isMicOn, setIsMicOn] = useState(true);
+  const [isCameraOn, setIsCameraOn] = useState(true);
+
+  const toggleCamera = useCallback(() => {
+    if (localStream) {
+      const videoTrack = localStream.getVideoTracks()[0];
+      if (videoTrack) {
+        setIsCameraOn((prev) => {
+          const newState = !prev;
+          videoTrack.enabled = newState;
+          return newState;
+        });
+      }
+    }
+  }, [localStream, isCameraOn]);
+
+  const toggleMic = useCallback(() => {
+    if (localStream) {
+      const audioTrack = localStream.getAudioTracks()[0];
+      if (audioTrack) {
+        setIsMicOn((prev) => {
+          const newState = !prev;
+          audioTrack.enabled = newState;
+          return newState;
+        });
+      }
+    }
+  }, [localStream, isMicOn]);
+
+  const endCall = useCallback(() => {
+    // Implement end call logic here
+  }, []);
   return (
     <div>
       {localStream && (
-        <VideoContainer
-          stream={localStream}
-          isLocalStream={true}
-          isOnCall={false}
-        />
+        <div className="flex flex-col justify-center items-center mt-4">
+          <VideoContainer
+            stream={localStream}
+            isLocalStream={true}
+            isOnCall={false}
+          />
+          <div className="flex gap-4 mt-4">
+            <button onClick={toggleMic}>
+              {isMicOn && <MdMic size={24} />}
+              {!isMicOn && <MdMicOff size={24} />}
+            </button>
+
+            <button onClick={toggleCamera}>
+              {isCameraOn && <MdVideocam size={24} />}
+              {!isCameraOn && <MdVideocamOff size={24} />}
+            </button>
+
+            <button
+              onClick={endCall}
+              className="w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center text-white"
+            >
+              <MdCallEnd size={24} />
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
